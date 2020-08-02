e1c4ba6218d9621e4b0df8283b11953c49656a6f
GCaplan <gabriel.caplan@gmail.com>
2020-08-02 03:43:19 -0400

    connected front end to db

--- server.js
+++ server.js
@@ -15,14 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-//OBJECT ARRAY TO STORE DATA=========
-// var notes = [
-//   {
-//     routeName: "tk",
-//     name: "tk",
-//   },
-// ];
-
 // HTLM ROUTES
 
 // //   * GET `/notes` - Should return the `notes.html` file.
@@ -35,28 +27,27 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-// The application should have a `db.json` file on the backend that will be used to store and retrieve notes using the `fs` module.
-let rawdata = fs.readFileSync("db/db.json");
-let db = JSON.parse(rawdata);
-console.log(db);
-
-// * The following API routes should be created:
-
-//   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+// * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 app.get("/api/notes", function (req, res) {
-  return db;
+  let rawdata = fs.readFileSync("db/db.json");
+  let db = JSON.parse(rawdata);
+  console.log(db);
+  return res.json(db);
 });
 
 //   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 
 app.post("/api/notes", function (req, res) {
+  let rawdata = fs.readFileSync("db/db.json");
+  let db = JSON.parse(rawdata);
   var newNote = req.body;
   console.log(newNote);
+  db.push(newNote);
+  fs.writeFileSync("db/db.json", JSON.stringify(db));
+  return res.json(newNote);
 });
 
 //   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-
-//reservations === GET/reservation html then POST
 
 // Starts the server to begin listening====================
 app.listen(PORT, function () {
