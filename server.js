db76b2bf5f0df2a698af1eac8858c5349261135c
GCaplan <gabriel.caplan@gmail.com>
2020-08-02 02:33:03 -0400

    expres app for static(public)

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var PORT = process.env.PORT || 3000;
 // // Sets up the Express app to handle data parsing========================
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 //OBJECT ARRAY TO STORE DATA=========
 // var notes = [
@@ -47,6 +48,11 @@ app.get("/api/notes", function (req, res) {
 });
 
 //   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+
+app.post("/api/notes", function (req, res) {
+  var newNote = req.body;
+  console.log(newNote);
+});
 
 //   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 
